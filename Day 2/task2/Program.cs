f7ada8893c797c53a0ec4164f5d1d3dc7f028ba6
Geleta116 <geleta.dabakumsa@gmail.com>
2023-08-02 10:12:49 +0300

    added the day 1 tasks

---
+++ Day 2/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WordFrequencyNamespace
+{
+    public class WordFrequencyCounter
+    {
+        public static Dictionary<string, int> CountFrequency(string phrase)
+        {   
+            phrase = phrase.Trim();
+            string[] separatedWords = phrase.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+
+            foreach (string word in separatedWords)
+            {
+                string cleanedWord = Regex.Replace(word, @"[^a-zA-Z0-9]", "").ToUpper();
+                if(cleanedWord != ""){
+                    if (counter.ContainsKey(cleanedWord))
+                    {
+                        counter[cleanedWord]++;
+                    }
+                    else
+                    {
+                        counter[cleanedWord] = 1;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> counts = CountFrequency("Hello Hello Hello 234  c  ; ??? sdf    ");
+            foreach (KeyValuePair<string, int> item in counts)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
