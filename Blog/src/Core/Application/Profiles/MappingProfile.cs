ee7886f0562352863ba0980b5676ebf63ec51169
Geleta116 <geleta.dabakumsa@gmail.com>
2023-08-15 11:51:32 +0300

    add the automapper

---
+++ Blog/src/Core/Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Blog.src.Core.Application.DTOs.CommentDtos;
+using Blog.src.Core.Application.DTOs.PostDtos;
+using Blog.src.Core.Domain.Entity;
+
+namespace Blog.src.Core.Application.Profiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Comment, CreateCommentDto>().ReverseMap();
+            CreateMap<Post, CreatePostDto>().ReverseMap();
+            CreateMap<Comment, UpdateCommentDto>().ReverseMap();
+            CreateMap<Post, UpdatePostDto>().ReverseMap();
+        }
+    }
+} 
